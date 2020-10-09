5faddd57f6f58b781d783b8d258c19bb2b6b3323
ow-tech <wagwanwekon@outlook.com>
2020-10-10 01:16:01 +0300

    created on click effect

--- js/script.js
+++ js/script.js
@@ -11,3 +11,13 @@ window.scroll({
     left: 0, 
     behavior: 'smooth' 
   });
+
+// Acrive and Remove menu class
+const navLink = document.querySelectorAll('.nav-link')
+
+function navLinkAction (){
+    navLink.forEach(n => n.classList.remove('active'))
+    this.classList.add('active')
+}
+
+navLink.forEach(n => n.addEventListener('click', navLinkAction))
